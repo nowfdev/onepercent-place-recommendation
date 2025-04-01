4118352e5c32e1e5906d35837bc1122f4535e712
nowfdev <nowf.dev@gmail.com>
2025-04-01 07:46:49 +0700

    downgrade to react v18 to fixing animations for side drawer

--- src/shared/components/Navigation/MainNavigation.jsx
+++ src/shared/components/Navigation/MainNavigation.jsx
@@ -3,54 +3,34 @@ import { Link } from "react-router-dom";
 import MainHeader from "./MainHeader";
 import SideDrawer from "./SideDrawer";
 import NavLinks from "./NavLinks";
+import Backdrop from "../UIElements/Backdrop";
 import "./MainNavigation.css";
 
 const MainNavigation = (props) => {
   const [drawerIsOpen, setDrawerIsOpen] = useState(false);
-  const sideDrawerRef = useRef();
 
-  const openDrawer = () => {
+  const openDrawerHandler = () => {
     setDrawerIsOpen(true);
   };
-  const closeDrawer = () => {
+  const closeDrawerHandler = () => {
     setDrawerIsOpen(false);
   };
 
-  // Close the drawer if a click is detected outside the drawer
-  useEffect(() => {
-    console.log(drawerIsOpen); // Logs the drawerIsOpen value when it changes
-
-    if (drawerIsOpen) {
-      const handleClickOutside = (event) => {
-        if (
-          sideDrawerRef.current &&
-          !sideDrawerRef.current.contains(event.target)
-        ) {
-          closeDrawer();
-        }
-      };
-
-      // Add event listener for click
-      document.addEventListener("mousedown", handleClickOutside);
-
-      // Cleanup the event listener on component unmount
-      return () => {
-        document.removeEventListener("mousedown", handleClickOutside);
-      };
-    }
-  }, [drawerIsOpen]);
-
   return (
     <React.Fragment>
-      {drawerIsOpen && (
-        <SideDrawer isOpen={drawerIsOpen} ref={sideDrawerRef}>
-          <nav className="main-navigation__drawer-nav">
-            <NavLinks />
-          </nav>
-        </SideDrawer>
-      )}
+      {drawerIsOpen && <Backdrop onClick={closeDrawerHandler} />}
+
+      <SideDrawer show={drawerIsOpen} onClick={closeDrawerHandler}>
+        <nav className="main-navigation__drawer-nav">
+          <NavLinks />
+        </nav>
+      </SideDrawer>
+
       <MainHeader>
-        <button className="main-navigation__menu-btn" onClick={openDrawer}>
+        <button
+          className="main-navigation__menu-btn"
+          onClick={openDrawerHandler}
+        >
           <span />
           <span />
           <span />
--- src/shared/components/Navigation/SideDrawer.jsx
+++ src/shared/components/Navigation/SideDrawer.jsx
@@ -1,32 +1,24 @@
-// import React from "react";
-// import "./SideDrawer.css";
-
-// const SideDrawer = (props) => {
-//   return <aside className="side-drawer">{props.children}</aside>;
-// };
-
-// export default SideDrawer;
-
 import React from "react";
+import ReactDOM from "react-dom";
 import { CSSTransition } from "react-transition-group";
 
 import "./SideDrawer.css";
 
-// Using forwardRef to pass ref to the root element of SideDrawer
-const SideDrawer = React.forwardRef((props, ref) => {
-  return (
+const SideDrawer = (props) => {
+  const content = (
     <CSSTransition
-      in={props.isOpen}
+      in={props.show}
       timeout={300}
       classNames="slide-in-left"
       mountOnEnter
       unmountOnExit
     >
-      <div ref={ref} className="side-drawer">
+      <aside className="side-drawer" onClick={props.onClick}>
         {props.children}
-      </div>
+      </aside>
     </CSSTransition>
   );
-});
+  return ReactDOM.createPortal(content, document.getElementById("drawer-hook"));
+};
 
 export default SideDrawer;
